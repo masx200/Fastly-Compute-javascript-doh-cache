53a295f3f8f20f7476a430aeb2495d3b38b0d860
masx200 <34191203+masx200@users.noreply.github.com>
2024-04-01 02:18:55 +0800

    Update .gitignore

--- build/base64Encode.js
+++
@@ -1,9 +0,0 @@
-export function base64Encode(byteArray) {
-    const buffer = new Uint8Array(byteArray);
-    const binaryString = buffer.reduce((str, byte) => str + String.fromCharCode(byte), "");
-    const encoded = btoa(binaryString)
-        .replace(/\+/g, "-")
-        .replace(/\//g, "_")
-        .replace(/=/g, "");
-    return encoded;
-}
